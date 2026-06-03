ab55cc460e06f23727f8e784396d90dc6caeea04
iamnotsnani <zeynallisnani@gmail.com>
2026-06-04 00:35:12 +0400

    dependencies added, app properties configured

---
+++ src/main/java/com/heartsync/HeartsyncApplication.java
@@ -0,0 +1,13 @@
+package com.heartsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeartsyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heartsync/HeartsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heartsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartsyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
