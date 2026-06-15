8c00c4ec6fd2a8fd49297e86711b9bd360325f22
iamnotsnani <zeynallisnani@gmail.com>
2026-06-16 03:51:14 +0400

    Custom exceptions added

---
+++ src/main/java/com/heartsync/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.heartsync.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
